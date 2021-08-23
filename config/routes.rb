dac11bcc172ad79f4c4e4814278caccda88835c4
tomoprogrammer <train.vegacat@gmail.com>
2021-08-23 13:58:28 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+ #get 'top' =>'homes#top'
+ 
+  root to: 'homes#top'
+  devise_for :users
+  
+  resources :books
+
+  #root :to => "homes#top"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
