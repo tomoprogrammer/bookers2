5cc43948e1aaab3d7c3d96ff58c13d9d81b55f53
tomoprogrammer <train.vegacat@gmail.com>
2021-09-06 14:02:57 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
-  
-   resources :users, only: [:show, :edit, :index, :update]
+
+   resources :users, only: [:show, :edit, :index, :update] do
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers' , as: 'followers'
+  end
+
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
