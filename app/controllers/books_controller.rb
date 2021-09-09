774a57b386b1a33f41f9bf65e9cd9a22163bf5dc
tomoprogrammer <train.vegacat@gmail.com>
2021-09-09 12:20:51 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
       @user = @book.user
       @book_new = Book.new
-      @book_comment = BookComment.new
+      @comment = BookComment.new
     end
 
     def edit
